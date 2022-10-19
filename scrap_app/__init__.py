1b175ee49d161bb66aafbcf3a1ba5641a53036f4
kunalchand30 <116072514+kunalchand30@users.noreply.github.com>
2022-10-19 11:26:47 +0530

    Update __init__.py

--- scrap_app/__init__.py
+++ scrap_app/__init__.py
@@ -5,24 +5,9 @@ from scrapy.crawler import CrawlerRunner
 BASE_DIR="/opt/disc_wt/"
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-        # DATABASE=os.path.join(app.instance_path, 'scrap_app.sqlite'),   # Default configuration settings for flask app shoud be changed with frontend lib
-    )
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app = Flask(__name__)
+    if __name__ == "__main__":
+        app.run()
 
     @app.route('/')
     def index():
@@ -54,13 +39,13 @@ def create_app(test_config=None):
                 subprocess.run(["mv", f"/root/crawlerenv/files/webscraper.cfg", "/root/.scrapy.cfg"]) #move Webscrapper.cfg file from crawlerenv to root due to scrapy arch will look root folder first 
                 subprocess.run(["scrapy", "crawl", "-O", os.path.join(tempfile.gettempdir(),'output.csv'), "-t", "csv", f"{request.form['website']}spider"]) #this is command for run scrapy file with template crwal and type csv.
 
-            # condition to select and run youtube api scraper    
-            if request.form['website'] in api_scraps: # this will select youtube scrapper from option
-                subprocess.run(["python3", f"{request.form['website']}_api_scraper.py", "-i", request.form['url'], "-a", request.form['auth_key']])
+#             # condition to select and run youtube api scraper    
+#             if request.form['website'] in api_scraps: # this will select youtube scrapper from option
+#                 subprocess.run(["python3", f"{request.form['website']}_api_scraper.py", "-i", request.form['url'], "-a", request.form['auth_key']])
             
             return send_file(os.path.join(tempfile.gettempdir(),'output.csv'))    
                     
-    return app  #start the app again
+#     return app  #start the app again
     
        
        
