9b3ad010a61caffd3d72bf8e70b41a2f1b829022
kunalchand30 <116072514+kunalchand30@users.noreply.github.com>
2022-10-20 11:34:57 +0530

    Update settings.py

--- scrappers/webscrapers/settings.py
+++ scrappers/webscrapers/settings.py
@@ -48,15 +48,21 @@ ROBOTSTXT_OBEY = False
 #SPIDER_MIDDLEWARES = {
 #    'webscrapers.middlewares.WebscrapersSpiderMiddleware': 543,
 # #}
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,                 #anable this is not work 50-54
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,                 #anable this is not work 50-54
+# }
 ROTATING_PROXY_LIST = [
     'proxy1.com:8000',
     'proxy2.com:8031',
     # ...
 ]
+DOWNLOADER_MIDDLEWARES = {
+    # ...
+    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+    # ...
+}
 # DOWNLOADER_MIDDLEWARES = {
 #             'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
 #                 'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
